c73c15548e43a97e1405556bcb8b3ed550843b3f
Phillip Choi <phillipchoi@Phillipui-MacBook-Pro.local>
2020-06-06 18:12:12 +0900

    fixed some

--- src/App.js
+++ src/App.js
@@ -17,13 +17,20 @@ class App extends Component {
       [0, 0, 0, 0],
     ];
     board = this.randomCoordinate(this.randomCoordinate(board));
+    let score = board.reduce((acc, cur) => {
+      return acc + cur.reduce((acc, cur) => acc + cur, 0);
+    }, 0);
 
-    this.setState({ board: board });
+    this.setState({ board, score });
   };
 
   randomNumberGenerator = () => {
-    const startNumber = [2, 4];
-    return startNumber[Math.floor(Math.random() * startNumber.length)];
+    // // 수정 전
+    // const startNumber = [2, 4];
+    // return startNumber[Math.floor(Math.random() * startNumber.length)];
+
+    // 수정 후
+    return Math.random() > 0.75 ? 4 : 2;
   };
 
   findEmptyCell = (board) => {
@@ -42,32 +49,52 @@ class App extends Component {
 
   randomCoordinate = (board) => {
     const emptyCell = this.findEmptyCell(board);
-    const randomNumberGenerator = this.randomNumberGenerator();
     const coordinate = emptyCell[Math.floor(Math.random() * emptyCell.length)];
-    board[coordinate[0]][coordinate[1]] = randomNumberGenerator;
+    // 직접 함수 리턴값을 넣어줌
+    board[coordinate[0]][coordinate[1]] = this.randomNumberGenerator();
 
     return board;
   };
 
   componentDidMount() {
-    document.body.addEventListener("keydown", this.keyPressed);
+    // 바로 moveCells 로 바인딩
+    document.body.addEventListener("keydown", this.moveCells);
+    // mount될시 init실행
+    this.init();
   }
 
-  keyPressed = (e) => {
-    this.moveCells(e.keyCode);
+  // // componentDidMount에서 변경되어서 필요없어짐.
+  // keyPressed = (e) => {
+  //   this.moveCells(e.keyCode);
+  // };
+
+  // 약간 맘에 안듬
+  addNewNumber = () => {
+    // randomCoordinate 함수가 state있는걸
+    // board[coordinate[0]][coordinate[1]] = randomNumber; 로 직접 변경을 함
+    const updatedBoard = this.randomCoordinate(this.state.board);
+    // score update
+    let score = this.state.board.reduce((acc, cur) => {
+      return acc + cur.reduce((acc, cur) => acc + cur, 0);
+    }, 0);
+
+    this.setState({
+      board: updatedBoard,
+      score,
+    });
   };
 
-  moveCells = (keyCode) => {
-    if (keyCode === 37) {
+  moveCells = (e) => {
+    if (e.keyCode === 37) {
       // left key pressed
       console.log("left key pressed!");
-    } else if (keyCode === 38) {
+    } else if (e.keyCode === 38) {
       // up key pressed
       console.log("up key pressed!");
-    } else if (keyCode === 39) {
+    } else if (e.keyCode === 39) {
       // right key pressed
       console.log("right key pressed!");
-    } else if (keyCode === 40) {
+    } else if (e.keyCode === 40) {
       // down key pressed
       console.log("down key pressed!");
     }
@@ -76,8 +103,8 @@ class App extends Component {
   render() {
     return (
       <div className='App' onKeyPress={this.keyPressed}>
-        <button onClick={() => this.init()}>GameStart</button>
-        <button onClick={() => this.randomCoordinate(this.state.board)}>
+        <button onClick={() => this.init()}>New Game</button>
+        <button onClick={() => this.addNewNumber(this.state.board)}>
           add one
         </button>
         <div>Score:{this.state.score}</div>
